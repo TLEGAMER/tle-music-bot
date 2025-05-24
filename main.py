2cf223d7b0a4ccb5cd60a567a915bf7e7b10fdec
TLEGAMER <149803823+TLEGAMER@users.noreply.github.com>
2025-05-25 05:40:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import discord
 from discord.ext import commands
 import yt_dlp
-from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -132,5 +131,4 @@ async def help_command(ctx):
 """
     await ctx.send(help_text)
 
-keep_alive()
 bot.run(os.getenv("TOKEN"))
